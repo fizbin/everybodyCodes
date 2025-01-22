f138d3fb4886c39816f6846df91bb42a182b5959
Daniel Martin <fizbin@gmail.com>
2025-01-21 20:22:04 -0500

    Quest 10 is done

--- ec2024/golang/quest10/main.go
+++ ec2024/golang/quest10/main.go
@@ -11,6 +11,11 @@ import (
 	"slices"
 )
 
+type point struct {
+	row int
+	col int
+}
+
 func main() {
 	flag.Parse()
 	argsWithoutProg := flag.Args()
@@ -74,7 +79,7 @@ func main() {
 		for scanner.Scan() {
 			grid = append(grid, []byte(scanner.Text()))
 		}
-		total := 0
+		points := make([]point, 0)
 		asterFinder := regexp.MustCompile(`[*][*][A-Z?]`)
 		sawNoAster := true
 		for idx, row := range grid {
@@ -83,83 +88,81 @@ func main() {
 			}
 			if sawNoAster {
 				for _, match := range asterFinder.FindAllIndex(row, -1) {
+					points = append(points, point{idx, match[0]})
 					sawNoAster = false
-					fillGridPass1(grid, idx, match[0])
 				}
 			} else if !slices.Contains(row, '*') {
 				sawNoAster = true
 			}
 		}
-		for _, row := range grid {
-			fmt.Println("1->:", string(row))
-		}
-		fmt.Println()
+		// for _, row := range grid {
+		// 	fmt.Println("1->:", string(row))
+		// }
+		// fmt.Println()
 
-		fillGridPass2(grid, 0, 48)
+		// rand.Shuffle(len(points), func(i, j int) { points[i], points[j] = points[j], points[i] })
 
 		madeChange := true
 		for madeChange {
 			madeChange = false
-			sawNoAster = true
-			for idx, row := range grid {
-				if idx+3 > len(grid) {
-					break
-				}
-				if sawNoAster {
-					for _, match := range asterFinder.FindAllIndex(row, -1) {
-						sawNoAster = false
-						if collectWord(grid, idx, match[0]) != nil {
-							madeChange = fillGridPass2(grid, idx, match[0]) || madeChange
-						}
+			for _, spot := range points {
+				if collectWord(grid, spot.row, spot.col) != nil {
+					before := ""
+					for idx := range 8 {
+						before += string(grid[spot.row+idx][spot.col:spot.col+8]) + "\n"
 					}
-				} else if !slices.Contains(row, '*') {
-					sawNoAster = true
+					fillGridPass1(grid, spot.row, spot.col)
+					madeChange = fillGridPass2(grid, spot.row, spot.col) || madeChange
+					if !crossCheckBlock(grid, spot.row, spot.col) {
+						after := ""
+						for idx := range 8 {
+							after += string(grid[spot.row+idx][spot.col:spot.col+8]) + "\n"
+						}
+						fmt.Println("ERR!", "\n"+before, "\n"+after)
+						panic("Inconsistent!")
+					}
 				}
 			}
-			if madeChange {
-				for _, row := range grid {
-					fmt.Println("2->:", string(row))
+			// Debugging
+			// if madeChange {
+			// 	for _, row := range grid {
+			// 		fmt.Println("2->:", string(row))
+			// 	}
+			// 	fmt.Println()
+			// }
+		}
+
+		total := 0
+		for _, spot := range points {
+			word := collectWord(grid, spot.row, spot.col)
+			wordPowr := 0
+			if word != nil {
+				wordPowr = wordPower(word)
+			}
+			if wordPowr != 0 {
+				total += wordPowr
+			} else {
+				for xpos := 2; xpos < 6; xpos++ {
+					for ypos := 2; ypos < 6; ypos++ {
+						grid[xpos+spot.row][ypos+spot.col] = '.'
+					}
 				}
-				fmt.Println()
+				fillGridPass1(grid, spot.row, spot.col)
+				if collectWord(grid, spot.row, spot.col) != nil {
+					fmt.Println("DBG New word ", string(collectWord(grid, spot.row, spot.col)), " at ", spot)
+				}
+				for xpos := 2; xpos < 6; xpos++ {
+					for ypos := 2; ypos < 6; ypos++ {
+						grid[xpos+spot.row][ypos+spot.col] = '.'
+					}
+				}
 			}
 		}
 
-		sawNoAster = true
-		for idx, row := range grid {
-			if idx+3 > len(grid) {
-				break
-			}
-			if sawNoAster {
-				for _, match := range asterFinder.FindAllIndex(row, -1) {
-					sawNoAster = false
-					word := collectWord(grid, idx, match[0])
-					if word != nil {
-						total += wordPower(word)
-					} else {
-						for xpos := 2; xpos < 6; xpos++ {
-							for ypos := 2; ypos < 6; ypos++ {
-								grid[xpos+idx][ypos+match[0]] = '.'
-							}
-						}
-						fillGridPass1(grid, idx, match[0])
-						if collectWord(grid, idx, match[0]) != nil {
-							fmt.Println("DBG New word ", string(collectWord(grid, idx, match[0])), " at ", idx, match[0])
-						}
-						for xpos := 2; xpos < 6; xpos++ {
-							for ypos := 2; ypos < 6; ypos++ {
-								grid[xpos+idx][ypos+match[0]] = '.'
-							}
-						}
-					}
-				}
-			} else if !slices.Contains(row, '*') {
-				sawNoAster = true
-			}
-		}
-
-		for _, row := range grid {
-			fmt.Println("3->:", string(row))
-		}
+		// Debugging
+		// for _, row := range grid {
+		// 	fmt.Println("3->:", string(row))
+		// }
 
 		fmt.Println("Part 3:", total)
 	}
@@ -180,6 +183,29 @@ func computeWord(grid [][]byte, offsetRow, offsetCol int) []byte {
 		}
 	}
 	return word
+}
+
+func crossCheckBlock(grid [][]byte, offsetRow, offsetCol int) bool {
+	seen := make(map[byte]bool)
+	for xpos := 2; xpos < 6; xpos++ {
+		charsRow := []byte{grid[xpos+offsetRow][0+offsetCol], grid[xpos+offsetRow][1+offsetCol], grid[xpos+offsetRow][6+offsetCol], grid[xpos+offsetRow][7+offsetCol]}
+		for ypos := 2; ypos < 6; ypos++ {
+			charsCol := []byte{grid[0+offsetRow][ypos+offsetCol], grid[1+offsetRow][ypos+offsetCol], grid[6+offsetRow][ypos+offsetCol], grid[7+offsetRow][ypos+offsetCol]}
+			me := grid[xpos+offsetRow][ypos+offsetCol]
+			if me >= 'A' && me <= 'Z' {
+				if seen[me] {
+					return false // log.Panic("Duplicate letter at ", xpos, ypos, " within block at ", offsetRow, offsetCol)
+				}
+				if !slices.Contains(charsRow, me) {
+					return false // log.Panic("Letter not in charsRow at ", xpos, ypos, " within block at ", offsetRow, offsetCol)
+				}
+				if !slices.Contains(charsCol, me) {
+					return false // log.Panic("Letter not in charsCol at ", xpos, ypos, " within block at ", offsetRow, offsetCol)
+				}
+			}
+		}
+	}
+	return true
 }
 
 // fills innards we know right off
@@ -207,7 +233,7 @@ func fillGridPass1(grid [][]byte, offsetRow, offsetCol int) {
 }
 
 func fillGridPass2(grid [][]byte, offsetRow, offsetCol int) bool {
-	madeChange := false
+	changes := make(map[point]byte)
 	for xpos := 2; xpos < 6; xpos++ {
 		charsRow := []byte{grid[xpos+offsetRow][0+offsetCol], grid[xpos+offsetRow][1+offsetCol], grid[xpos+offsetRow][6+offsetCol], grid[xpos+offsetRow][7+offsetCol]}
 		for ypos := 2; ypos < 6; ypos++ {
@@ -221,85 +247,84 @@ func fillGridPass2(grid [][]byte, offsetRow, offsetCol int) bool {
 					}
 				}
 				if nQuestionMarks > 1 {
-					continue
+					return false
 				}
-				if nQuestionMarks == 0 {
-					foundSpot := false
+				if slices.Contains(charsRow, '?') {
+					colcontents := make([]byte, 0)
+					for xpos2 := 2; xpos2 < 6; xpos2++ {
+						colcontents = append(colcontents, grid[xpos2+offsetRow][ypos+offsetCol])
+					}
+					var replacementChar byte
 					for _, ch := range charsCol {
-						if slices.Contains(charsRow, ch) {
-							grid[xpos+offsetRow][ypos+offsetCol] = ch
-							foundSpot = true
-							break
-						}
-					}
-					if !foundSpot {
-						grid[xpos+offsetRow][ypos+offsetCol] = '%'
-					}
-				} else {
-					if slices.Contains(charsRow, '?') {
-						colcontents := make([]byte, 0)
-						for xpos2 := 2; xpos2 < 6; xpos2++ {
-							colcontents = append(colcontents, grid[xpos2+offsetRow][ypos+offsetCol])
-						}
-						var replacementChar byte
-						for _, ch := range charsCol {
-							if !slices.Contains(colcontents, ch) {
-								if replacementChar == 0 {
-									replacementChar = ch
-								} else {
-									replacementChar = 0
-									break
-								}
+						if !slices.Contains(colcontents, ch) {
+							if replacementChar == 0 {
+								replacementChar = ch
+							} else {
+								replacementChar = 0
+								break
 							}
 						}
-						if replacementChar == 0 {
-							continue
-						}
-						grid[xpos+offsetRow][ypos+offsetCol] = replacementChar
+					}
+					if replacementChar == 0 {
+						return false
+					}
+					changes[point{xpos + offsetRow, ypos + offsetCol}] = replacementChar
+					if !slices.Contains(charsRow, replacementChar) {
 						for _, ypos2 := range []int{0, 1, 6, 7} {
 							if grid[xpos+offsetRow][ypos2+offsetCol] == '?' {
-								grid[xpos+offsetRow][ypos2+offsetCol] = replacementChar
+								changes[point{xpos + offsetRow, ypos2 + offsetCol}] = replacementChar
 							}
 						}
-					} else {
-						// slices.Contains(charsCol, '?')
-						rowcontents := grid[xpos+offsetRow][2+offsetCol : 6+offsetCol]
-						var replacementChar byte
-						for _, ch := range charsRow {
-							if !slices.Contains(rowcontents, ch) {
-								if replacementChar == 0 {
-									replacementChar = ch
-								} else {
-									replacementChar = 0
-									break
-								}
+					}
+				} else {
+					// slices.Contains(charsCol, '?')
+					rowcontents := grid[xpos+offsetRow][2+offsetCol : 6+offsetCol]
+					var replacementChar byte
+					for _, ch := range charsRow {
+						if !slices.Contains(rowcontents, ch) {
+							if replacementChar == 0 {
+								replacementChar = ch
+							} else {
+								replacementChar = 0
+								break
 							}
 						}
-						if replacementChar == 0 {
-							continue
-						}
-						grid[xpos+offsetRow][ypos+offsetCol] = replacementChar
+					}
+					if replacementChar == 0 {
+						return false
+					}
+					changes[point{xpos + offsetRow, ypos + offsetCol}] = replacementChar
+					if !slices.Contains(charsCol, replacementChar) {
 						for _, xpos2 := range []int{0, 1, 6, 7} {
 							if grid[xpos2+offsetRow][ypos+offsetCol] == '?' {
-								grid[xpos2+offsetRow][ypos+offsetCol] = replacementChar
+								changes[point{xpos2 + offsetRow, ypos + offsetCol}] = replacementChar
 							}
 						}
 					}
 				}
-				madeChange = true
 			}
 		}
 	}
-	return madeChange
+	for spot, val := range changes {
+		grid[spot.row][spot.col] = val
+	}
+	return len(changes) > 0
 }
 
 func collectWord(grid [][]byte, offsetRow, offsetCol int) []byte {
 	retval := make([]byte, 0)
+	seenLetters := make(map[byte]bool)
 	for xpos := 2; xpos < 6; xpos++ {
 		for ypos := 2; ypos < 6; ypos++ {
 			me := grid[xpos+offsetRow][ypos+offsetCol]
 			if me == '%' {
 				return nil
+			}
+			if me != '.' {
+				if seenLetters[me] {
+					return nil
+				}
+				seenLetters[me] = true
 			}
 			retval = append(retval, me)
 		}
@@ -307,99 +332,11 @@ func collectWord(grid [][]byte, offsetRow, offsetCol int) []byte {
 	return retval
 }
 
-// computeWordP3 can return nil; also, it will adjust ? values in grid as needed
-func computeWordP3(grid [][]byte, offsetRow, offsetCol int) []byte {
-	dotsx := make([]int, 0)
-	dotsy := make([]int, 0)
-	for xpos := 2; xpos < 6; xpos++ {
-		charsRow := []byte{grid[xpos+offsetRow][0+offsetCol], grid[xpos+offsetRow][1+offsetCol], grid[xpos+offsetRow][6+offsetCol], grid[xpos+offsetRow][7+offsetCol]}
-		for ypos := 2; ypos < 6; ypos++ {
-			charsCol := []byte{grid[0+offsetRow][ypos+offsetCol], grid[1+offsetRow][ypos+offsetCol], grid[6+offsetRow][ypos+offsetCol], grid[7+offsetRow][ypos+offsetCol]}
-			foundSpot := false
-			for _, ch := range charsCol {
-				if ch == '?' {
-					continue
-				}
-				if slices.Contains(charsRow, ch) {
-					grid[xpos+offsetRow][ypos+offsetCol] = ch
-					foundSpot = true
-					break
-				}
-			}
-			if !foundSpot && !slices.Contains(charsCol, '?') && !slices.Contains(charsRow, '?') {
-				grid[xpos+offsetRow][ypos+offsetCol] = '%'
-				return nil
-			} else if !foundSpot {
-				dotsx = append(dotsx, xpos)
-				dotsy = append(dotsy, ypos)
-			}
-		}
-	}
-	if len(dotsx) > 0 {
-
-		// fmt.Println("DBG-cwp3")
-		// for xpos := 0; xpos < 8; xpos++ {
-		// 	for ypos := 0; ypos < 8; ypos++ {
-		// 		fmt.Print(string(grid[xpos+offsetRow][ypos+offsetCol : ypos+offsetCol+1]))
-		// 	}
-		// 	fmt.Println()
-		// }
-		// fmt.Println()
-		for idx, xpos := range dotsx {
-			ypos := dotsy[idx]
-			charsRow := []byte{grid[xpos+offsetRow][0+offsetCol], grid[xpos+offsetRow][1+offsetCol], grid[xpos+offsetRow][6+offsetCol], grid[xpos+offsetRow][7+offsetCol]}
-			charsCol := []byte{grid[0+offsetRow][ypos+offsetCol], grid[1+offsetRow][ypos+offsetCol], grid[6+offsetRow][ypos+offsetCol], grid[7+offsetRow][ypos+offsetCol]}
-			// if slices.Contains(charsRow, '?') == slices.Contains(charsCol, '?') {
-			// 	log.Fatal("Expected exactly one question mark", string(charsRow), string(charsCol), offsetRow, offsetCol, "@", xpos, ypos)
-			// }
-			// fmt.Println("DBG-cwp3.charsRow: ", string(charsRow))
-			// fmt.Println("DBG-cwp3.charsCol: ", string(charsCol))
-			if slices.Contains(charsRow, '?') {
-				colcontents := make([]byte, 0)
-				for xpos2 := 2; xpos2 < 6; xpos2++ {
-					colcontents = append(colcontents, grid[xpos2+offsetRow][ypos+offsetCol])
-				}
-				// fmt.Println("DBG-cwp3.colcontents: ", string(colcontents))
-				for _, ch := range charsCol {
-					if !slices.Contains(colcontents, ch) {
-						grid[xpos+offsetRow][ypos+offsetCol] = ch
-						for _, ypos2 := range []int{0, 1, 6, 7} {
-							if grid[xpos+offsetRow][ypos2+offsetCol] == '?' {
-								grid[xpos+offsetRow][ypos2+offsetCol] = ch
-							}
-						}
-						break
-					}
-				}
-			} else {
-				// slices.Contains(charsCol, '?')
-				rowcontents := grid[xpos+offsetRow][2+offsetCol : 6+offsetCol]
-				for _, ch := range charsRow {
-					if !slices.Contains(rowcontents, ch) {
-						grid[xpos+offsetRow][ypos+offsetCol] = ch
-						for _, xpos2 := range []int{0, 1, 6, 7} {
-							if grid[xpos2+offsetRow][ypos+offsetCol] == '?' {
-								grid[xpos2+offsetRow][ypos+offsetCol] = ch
-							}
-						}
-						break
-					}
-				}
-			}
-		}
-	}
-	word := make([]byte, 0)
-	for xpos := 2; xpos < 6; xpos++ {
-		word = slices.Concat(word, grid[xpos+offsetRow][2+offsetCol:6+offsetCol])
-	}
-	return word
-}
-
 func wordPower(word []byte) int {
 	retval := 0
 	for idx, v := range word {
 		if v == '.' {
-			log.Fatal("Bad word: ", string(word))
+			log.Fatal("Bad word ", string(word))
 		}
 		retval += (idx + 1) * int(v-'A'+1)
 	}
